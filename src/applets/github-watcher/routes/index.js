18ee4cb98e458d133d6d7933f38a5ed01c9a8435
michal-cic <michal.cic@gmail.com>
2020-04-11 20:20:17 +0200

    refactored routes and utils

--- src/applets/github-watcher/routes/index.js
+++ src/applets/github-watcher/routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const nanoid = require('nanoid');
 
 const { ensureLoggedIn } = require('../../../utils');
-const { APPLET_ID, AUTH_URL, exchangeCodeForAccessToken } = require('../utils');
+const {
+  APPLET_ID,
+  AUTH_URL,
+  exchangeCodeForAccessToken,
+  createWebhook,
+  removeWebhook,
+  validateRepoName
+} = require('../utils');
 const userAppletsRepo = require('../../../repositories/userApplets');
 
 router.get(
@@ -17,11 +25,85 @@ router.get(
           userId
         });
       } catch (err) {
-        // Do nothing. Continue.
+        // Do nothing? Okay.
       }
       res.render('github-watcher/views/index', {
         userApplet
       });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  '/applets/github-watcher/authorize',
+  ensureLoggedIn,
+  async (req, res, next) => {
+    try {
+      const { repository } = req.body;
+      validateRepoName(repository);
+      res.redirect(`${AUTH_URL}/${repository}`);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  '/applets/github-watcher/subscribe/:owner/:repo',
+  ensureLoggedIn,
+  async (req, res, next) => {
+    try {
+      const { code } = req.query;
+      const { owner, repo } = req.params;
+      const repository = `${owner}/${repo}`;
+      const { id: userId } = req.user;
+      const identifier = nanoid(64);
+      const {
+        access_token: providerAccessToken
+      } = await exchangeCodeForAccessToken(code);
+      const { id: hookId } = await createWebhook({
+        repository,
+        providerAccessToken,
+        identifier
+      });
+      await userAppletsRepo.createUserApplet({
+        appletId: APPLET_ID,
+        configuration: JSON.stringify({
+          repository: repository,
+          hookId
+        }),
+        providerAccessToken,
+        identifier,
+        userId
+      });
+      req.flash(
+        'success',
+        'You are now subscribed to the GitHub commit watcher.'
+      );
+      res.redirect('/');
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.post(
+  '/applets/github-watcher/unsubscribe/:identifier',
+  ensureLoggedIn,
+  async (req, res, next) => {
+    try {
+      const { providerAccessToken, config } = req.body;
+      const { repository, hookId } = JSON.parse(config);
+      const { identifier } = req.params;
+      await removeWebhook({ repository, hookId, providerAccessToken });
+      await userAppletsRepo.deleteUserAppletByIdentifier(identifier);
+      req.flash(
+        'success',
+        "You just unsubscribed from GitHub watcher. It's sad to see you go."
+      );
+      res.redirect('/');
     } catch (err) {
       next(err);
     }
--- src/applets/github-watcher/utils.js
+++ src/applets/github-watcher/utils.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
-const qs = require('querystring');
+const createError = require('http-errors');
 
-const { db } = require('../../clients');
+const { sg } = require('../../clients');
+
+const userAppletRepo = require('../../repositories/userApplets');
+const userRepo = require('../../repositories/users');
 
 const APPLET_ID = 3;
 const SCOPES = 'repo';
@@ -11,32 +14,116 @@ const REDIRECT_URI = process.env.APPLET_GITHUB_WATCHER_REDIRECT_URI;
 const AUTH_URL = [
   'https://github.com/login/oauth/authorize/',
   `?client_id=${CLIENT_ID}`,
-  `&redirect_uri=${REDIRECT_URI}`,
-  `&scope=${SCOPES}`
-].join(''); // TODO add state for an extra security layer
+  `&scope=${SCOPES}`,
+  `&redirect_uri=${REDIRECT_URI}`
+].join('');
 
 const exchangeCodeForAccessToken = async code => {
-  const response = await axios.post(
-    'https://github.com/login/oauth/access_token',
-    null,
-    {
-      params: {
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        code: code,
-        redirect_uri: REDIRECT_URI
-      },
-      headers: {
-        Accept: 'application/json'
+  let response;
+  try {
+    response = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      undefined,
+      {
+        params: {
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          code: code,
+          redirect_uri: REDIRECT_URI
+        },
+        headers: {
+          Accept: 'application/json'
+        }
       }
-    }
-  );
+    );
+  } catch (err) {
+    throw createError(500, err);
+  }
   console.log(`GitHub access token received: ${JSON.stringify(response.data)}`);
   return response.data;
+};
+
+const createWebhook = async ({
+  repository,
+  providerAccessToken,
+  identifier
+}) => {
+  let response;
+  try {
+    response = await axios.post(
+      `https://api.github.com/repos/${repository}/hooks`,
+      {
+        name: 'web',
+        active: true,
+        events: ['push'],
+        config: {
+          url: `https://704ae309.ngrok.io/api/applets/github-watcher/webhook/${identifier}`, // TODO change uri
+          content_type: 'json',
+          insecure_ssl: '0'
+        }
+      },
+      {
+        headers: {
+          Authorization: `token ${providerAccessToken}`
+        }
+      }
+    );
+  } catch (err) {
+    throw createError(500, err);
+  }
+  return response.data;
+};
+
+const removeWebhook = async ({ repository, hookId, providerAccessToken }) => {
+  try {
+    await axios.delete(
+      `https://api.github.com/repos/${repository}/hooks/${hookId}`,
+      {
+        headers: {
+          Authorization: `token ${providerAccessToken}`
+        }
+      }
+    );
+  } catch (err) {
+    throw createError(500, err);
+  }
+};
+
+const validateRepoName = repoName => {
+  const pattern = /\u002F/g; // searches for '/' in a string
+  const match = repoName.match(pattern); // returns an array of matched characters -> "owner/repo" = ["/"]; "owner/repo/hook" = ["/", "/"]; "owner+repo" = null
+  console.log(match);
+  if (match == null || match.length !== 1) {
+    throw createError(500, 'Invalid repository name format');
+  }
+};
+
+const sendMail = async ({ identifier, body }) => {
+  const userApplet = await userAppletRepo.findUserAppletByIdentifier(
+    identifier
+  );
+  const user = await userRepo.findUserById(userApplet.userId);
+  if (body.head_commit !== undefined) {
+    await sg.send({
+      to: user.email,
+      from: process.env.SG_FROM_EMAIL,
+      templateId: 'd-a451c67c185f447eb905fe85055dc003',
+      dynamic_template_data: {
+        committer: body.head_commit.committer.username,
+        repository: body.repository.full_name
+      }
+    });
+  } else {
+    // here comes the ping from github to confirm webhook creation
+  }
 };
 
 module.exports = {
   APPLET_ID,
   AUTH_URL,
-  exchangeCodeForAccessToken
+  exchangeCodeForAccessToken,
+  createWebhook,
+  removeWebhook,
+  validateRepoName,
+  sendMail
 };
