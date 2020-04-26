b2e7d35883f799861a4a170b53c9a5af79714277
michal-cic <michal.cic@gmail.com>
2020-04-26 17:18:01 +0200

    rearranged routes

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ app.use(require('./src/applets/covid19-report-discord/routes/api'));
 app.use(require('./src/applets/covid19-report-mail/routes/api'));
 app.use(require('./src/applets/dropbox-watcher/routes/api'));
 app.use(require('./src/applets/github-watcher/routes/api'));
-app.use(require('./src/applets/weather-report-mail/routes/api'));
 app.use(require('./src/applets/weather-report-gsheet/routes/api'));
+app.use(require('./src/applets/weather-report-mail/routes/api'));
 
 // Enforce CSRF protection
 app.use(csrf({ cookie: true }));
@@ -76,8 +76,8 @@ app.use(require('./src/applets/covid19-report-discord/routes'));
 app.use(require('./src/applets/covid19-report-mail/routes'));
 app.use(require('./src/applets/dropbox-watcher/routes'));
 app.use(require('./src/applets/github-watcher/routes'));
-app.use(require('./src/applets/weather-report-mail/routes'));
 app.use(require('./src/applets/weather-report-gsheet/routes'));
+app.use(require('./src/applets/weather-report-mail/routes'));
 
 // 404 Handler
 app.use((req, res, next) => {
